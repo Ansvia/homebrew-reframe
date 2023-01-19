275d7aee8bf559f90f6ac87e692e4982120187e3
Robin Syihab <obin.mf@gmail.com>
2023-01-19 21:13:17 +0700

    update to reframe v0.5.5

--- Formula/reframe.rb
+++ Formula/reframe.rb
@@ -3,9 +3,9 @@
 class Reframe < Formula
   desc "Light, Fast & Simple project scaffolder"
   homepage "https://github.com/Ansvia/reframe"
-  url "https://github.com/Ansvia/reframe/releases/download/v0.5.1/reframe_v0.5.1-x86_64-darwin.zip"
-  sha256 "1a3a249e5db37b4da71f1889b67671cf7c36bea0da45c59c555632f02b543342"
-  version "0.5.1"
+  url "https://github.com/Ansvia/reframe/releases/download/v0.5.5/reframe_v0.5.5-x86_64-darwin.zip"
+  sha256 "d5043c439a7ea6aaf5347accef122560b99219351b52002a9464ed268804d2bc"
+  version "0.5.5"
 
   def install
     bin.install "reframe"
